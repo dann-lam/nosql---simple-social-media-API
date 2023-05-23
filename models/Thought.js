0c3079c90bf266f214066d4f17f372dcd00d6269
Daniel Lam <Dann.lam@gmail.com>
2023-05-22 23:34:29 -0700

    Stuff is now seeded

--- models/Thought.js
+++ models/Thought.js
@@ -1,35 +1,31 @@
 const { Schema, model } = require("mongoose");
 const reactionSchema = require("./Reaction");
-//a timestamp getter
 
-// function timeStampQuery(createdAt){
-//     return;
-// }
-
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    // get: timestampQuery(createdAt),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema],
-
-  toJSON: {
-    virtuals: true,
-    // getters: true,
-  },
-  id: false,
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
--- utils/seed.js
+++ utils/seed.js
@@ -1,51 +1,36 @@
-const connection = require('../config/connection');
-const { Course, Student } = require('../models');
-const { getRandomName, getRandomAssignments } = require('./data');
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const { userMulti, thoughtMulti } = require("./data");
 
-connection.on('error', (err) => err);
+connection.on("error", (err) => err);
 
-connection.once('open', async () => {
-  console.log('connected');
+connection.once("open", async () => {
+  console.log("connected");
 
   // Drop existing courses
-  await Course.deleteMany({});
+  await User.deleteMany({});
 
   // Drop existing students
-  await Student.deleteMany({});
+  await Thought.deleteMany({});
 
-  // Create empty array to hold the students
-  const students = [];
+  await User.insertMany(userMulti);
 
-  // Loop 20 times -- add students to the students array
-  for (let i = 0; i < 20; i++) {
-    // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
-
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
-    students.push({
-      first,
-      last,
-      github,
-      assignments,
-    });
+  for (const thought of thoughtMulti) {
+    const createdThought = await Thought.create(thought);
+    console.log(createdThought);
+    const updatedUser = await User.findOneAndUpdate(
+      {
+        $or: [
+          { username: createdThought.username },
+          { _id: createdThought.userId },
+        ],
+      },
+      { $addToSet: { thoughts: createdThought._id } },
+      { runValidators: true, new: true }
+    );
+    console.log(updatedUser);
   }
 
-  // Add students to the collection and await the results
-  await Student.collection.insertMany(students);
-
-  // Add courses to the collection and await the results
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
-    inPerson: false,
-    students: [...students],
-  });
-
-  // Log out the seed data to indicate what should appear in the database
-  console.table(students);
-  console.info('Seeding complete! 🌱');
+  console.info("Seeding complete! 🌱");
   process.exit(0);
 });
