2ed1fd5290cfd5e53889120f20bf5859d2c3652d
Daniel Lam <Dann.lam@gmail.com>
2023-05-22 20:44:07 -0700

    User and Index model added

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      //This match came from this stackoverflow thread: https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+//friendCount virtual
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
