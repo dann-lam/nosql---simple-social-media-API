a2453bd369cfe3aad0b5a133ede1105f0c171481
Daniel Lam <Dann.lam@gmail.com>
2023-05-22 23:00:01 -0700

    seeds added

---
+++ utils/data.js
@@ -0,0 +1,35 @@
+const userMulti = [
+  {
+    username: "user_one",
+    email: "user_one@gmail.com",
+  },
+  {
+    username: "user_two",
+    email: "user_two@gmail.com",
+  },
+  {
+    username: "user_three",
+    email: "user_three@gmail.com",
+  },
+];
+
+const thoughtMulti = [
+  {
+    thoughtText: "user_one_thought_one",
+    username: "user_one",
+  },
+  {
+    thoughtText: "user_one_thought_two",
+    username: "user_one",
+  },
+  {
+    thoughtText: "user_two_thought_one",
+    username: "user_two",
+  },
+  {
+    thoughtText: "user_three_thought_one",
+    username: "user_three",
+  },
+];
+
+module.exports = { userMulti, thoughtMulti };
